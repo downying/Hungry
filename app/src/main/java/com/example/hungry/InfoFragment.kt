35e9214324900846a9ee487b58a1db7f35f4b2bc
downy <tkwk36@naver.com>
2023-07-23 20:52:10 +0900

    upload

--- app/src/main/java/com/example/hungry/InfoFragment.kt
+++ app/src/main/java/com/example/hungry/InfoFragment.kt
@@ -1,40 +1,34 @@
 package com.example.hungry
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
-import com.example.hungry.databinding.FragmentMypageBinding
+import com.example.hungry.databinding.FragmentInfoBinding
 
-class MypageFragment : Fragment() {
+class InfoFragment : Fragment() {
 
-    lateinit var binding: FragmentMypageBinding
+    lateinit var binding: FragmentInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMypageBinding.inflate(inflater, container, false)
+        binding = FragmentInfoBinding.inflate(inflater, container, false)
 
-        binding.btnLogin.setOnClickListener {
+
+        binding.btnOut.setOnClickListener {
             val homeFragment = HomeFragment()
             val transaction: FragmentTransaction = parentFragmentManager.beginTransaction()
             transaction.replace(R.id.main_frm, homeFragment)
             transaction.addToBackStack(null)
             transaction.commit()
         }
-
-        binding.btnInformation.setOnClickListener {
-            val infoFragment = InfoFragment()
-            val transaction: FragmentTransaction = parentFragmentManager.beginTransaction()
-            transaction.replace(R.id.main_frm, infoFragment)
-            transaction.addToBackStack(null)
-            transaction.commit()
-        }
-
 
         return binding.root
     }
